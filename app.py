cbc24d038665a99dc9d6215274e92a420fa4c011
J19Z <jgmusso@gmail.com>
2024-03-09 20:43:39 -0500

    Some changes to the code and added the requirements file

--- app.py
+++ app.py
@@ -3,47 +3,63 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from dotenv import find_dotenv, load_dotenv
-from transformers import pipeline
+#from transformers import pipeline
 import requests
 import streamlit as st
 
 load_dotenv(find_dotenv())
 HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
+headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
+
 
 # img2text
 def img2text(url):
-    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-    #text = image_to_text(url) # [{'generated_text': 'a man with a beard and a blue jacket'}]
-    text = image_to_text(url)[0]['generated_text']
-    return text
+    # This is using HuggingFace servers
+    API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-base"
+    def query(filename):
+        with open(filename, "rb") as f:
+            data = f.read()
+        response = requests.post(API_URL, headers=headers, data=data)
+        return response.json()
+
+    output = query(url)
+    return output[0]['generated_text']
+    
+    ## This code is using the Pipeline, this is local 
+
+    # image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    # #text = image_to_text(url) # [{'generated_text': 'a man with a beard and a blue jacket'}]
+    # text = image_to_text(url)[0]['generated_text']
+    # return text
+
 
 # LLM
 def create_story(text):
-    #text_to_story = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta")
-    #story = text2speach
     API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
-    # def query(payload):
-    #     response = requests.post(API_URL, headers=headers, json=payload)
-    #     return response.json()
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
     
-    input = f'Create a short 50 word story using this as an input: "{text}"'
-    payloads = {
-        "inputs": input,
-    }
-    response = requests.post(API_URL, headers=headers, json=payloads)
-    return response.json()[0]['generated_text'].split('"')[2]
+    input_prompt = f'Create a short 50 word story using this as an input: "{text}"'
+    output = query({
+        "inputs": input_prompt,
+    })
+
+    generated_text = output[0]['generated_text']
+    return generated_text.replace(input_prompt, "").strip()
+
 
 # text to speech
 def text2speach(message):
     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
+
     payloads = {
         'inputs': message
     }
     response = requests.post(API_URL, headers=headers, json=payloads)
     with open('audio.flac', 'wb') as file:
         file.write(response.content)
+
 
 def main():
     st.set_page_config(page_title="Image 2 Audio Story", page_icon='🍌')
@@ -60,9 +76,10 @@ def main():
         story = create_story(text)
         text2speach(story)
 
-        with st.expander('Story'):
+        with st.expander('Story pic'):
             st.write(story)
         st.audio('audio.flac')
+
 
 if __name__ == '__main__':
     main()
